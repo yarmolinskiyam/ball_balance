8c544fcb3e74d63ee8b8835a9b23b00596b246b9
Arseniy <yarmolinskiyam@gmail.com>
2023-07-08 17:58:11 +0300

    Сонар в Serial

--- pio_ball_balance/src/main.cpp
+++ pio_ball_balance/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#include <Ultrasonic.h>
+#include <ArduinoLog.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ARDUINO_LOGS_ENABLE_COLORS
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Ultrasonic sonar (6, 7, 1000000/330);
+
+void setup()
+{
+  Serial.begin (9600);
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  uint32_t timeBefore = micros ();
+  unsigned int distance = sonar.read ();
+  uint32_t timeAfter = micros ();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (distance > 40);
+    // Log.warning ("dist = %d cm" CR, distance);
+  else
+    Log.notice ("dist = %d cm" CR, distance);
 }
